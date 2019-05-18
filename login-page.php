d22cec3fa87e72125b87708ed9a751b96b4482ca
Navanath <webempire143@gmail.com>
2019-05-18 13:13:28 -0700

    User login form and validation completed successfully

---
+++ login-page.php
@@ -0,0 +1,58 @@
+<?php
+
+	$db_connection = mysqli_connect( "localhost", "root", "", "login" );
+
+	if ( isset( $_POST["login_btn"] ) ) {
+
+		$username = $_POST["username"];
+		$password = $_POST["password"];
+
+		$sql_query = "select * from users where username='". $username ."' and password='". $password ."'";
+
+		$users_data_result = mysqli_query( $db_connection, $sql_query );
+
+		if ( mysqli_num_rows( $users_data_result ) > 0 ) {
+			echo "<script> window.location.href='registration.php'; </script>";
+		} else {
+			echo "<script>window.alert('Invalid Username or Password'); window.location.href='login-page.php';</script>";
+		}
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Login Page </title>
+
+		<link href="login-style.css" rel="stylesheet" id="login-css">
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	</head>
+	<body>
+
+		<div class="wrapper fadeInDown">
+		  <div id="formContent">
+		    
+			<div class="fadeIn first">
+		    	<img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
+		    	<h4 class="page-title">Login Page</h4>
+		    </div>
+
+
+			<form method="post">
+				<input type="text" class="fadeIn second" name="username" placeholder="Enter Username" />
+				<input type="text" class="fadeIn third" name="password" placeholder="Enter Password" />
+				<input type="submit" class="fadeIn fourth" value="Log In" name="login_btn" />
+			</form>
+
+			<div id="formFooter">
+				<a class="underlineHover" href="#">Forgot Password?</a>
+			</div>
+
+			</div>
+		</div>
+
+	</body>
+</html> 
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Q. Create a database, create `users` table in it. Then from HTML, CSS, PHP create a login form (can use predefined username, password for admin) after admin login there will be another form open for adding user, this form will contain 2 inputs one for username and another will be for password after clicking *Add User* button new user will be added to that login portal and his username, password will save in your DB's `users` table.
+
+Following points will have grace marks.
+
+1. Elegant login form
+2. AJAX implementation for adding user 
+3. User encryption for saving user's passwords in DB
